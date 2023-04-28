7417f092e415ec06e8d3241eacdda4e3495fdc39
Santiago Saavedra <mssr.99@hotmail.com>
2023-04-28 12:35:11 -0400

    change escenario to an Atari one

--- environment/luna_lander/create_environment.py
+++ environment/luna_lander/create_environment.py
@@ -2,20 +2,20 @@ import gym
 
 
 def luna_lander_normal():
+    
     from models.luna_lander import model
-    env = gym.make("LunarLander-v2", render_mode="human",
-                   enable_wind=True, wind_power=15)
+    env = gym.make("ALE/DemonAttack-v5", render_mode="human")
 
-    n_states = env.observation_space.shape[0]
     n_actions = env.action_space.n
-    model_sarsa = model.SarsaModel(n_states, n_actions)
-    for episode in range(500):
+    n_states = env.observation_space.shape[0]
+    model_sarsa = model.SarsaModel(n_states, n_actions)    
+    for episode in range(2000):
         rewards = 0
 
-        observation = env.reset()
-        state = 3
+        state = env.reset()
         action = model_sarsa.get_action(state, episode, env)
         for step in range(400):
+            env.render()
             next_observation, reward, terminated, truncated, info = env.step(action)
             next_action = model_sarsa.get_action(state, episode, env)
             next_state = model_sarsa.convert_state(next_observation)
