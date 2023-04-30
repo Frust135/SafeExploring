3159b05afb90587d945eb8dc3ee29ba35e47a270
Santiago Saavedra <mssr.99@hotmail.com>
2023-04-30 11:24:28 -0400

    remove second enviorenment

--- environment/luna_lander/create_environment.py
+++ environment/luna_lander/create_environment.py
@@ -1,36 +1,8 @@
+import numpy as np
 import gym
 
-
 def luna_lander_normal():
-    
-    from models.luna_lander import model
-    env = gym.make("ALE/DemonAttack-v5", render_mode="human")
-
-    n_actions = env.action_space.n
-    n_states = env.observation_space.shape[0]
-    model_sarsa = model.SarsaModel(n_states, n_actions)    
-    for episode in range(2000):
-        rewards = 0
-
-        state = env.reset()
-        action = model_sarsa.get_action(state, episode, env)
-        for step in range(400):
-            env.render()
-            next_observation, reward, terminated, truncated, info = env.step(action)
-            next_action = model_sarsa.get_action(state, episode, env)
-            next_state = model_sarsa.convert_state(next_observation)
-            model_sarsa.update(state, action, reward, next_state, next_action)
-            if terminated or truncated:
-                break
-            
-            action = next_action
-            state = next_state            
-            rewards += reward
-        text = 'Episodio: {0} - Recompensa acumulada: {1}'.format(
-            episode, rewards)
-        print(text)
-    return True
-
+    pass
 
 def luna_lander_controlled():
     pass
--- models/luna_lander/model.py
+++ models/luna_lander/model.py
@@ -2,8 +2,8 @@ import numpy as np
 
 
 class SarsaModel():
-    def __init__(self, n_states, n_actions):
-        self.n_states = self.get_states()
+    def __init__(self, n_states, n_actions):        
+        self.n_states = n_states
         self.n_actions = n_actions
 
         self.alpha = 0.1
@@ -12,29 +12,20 @@ class SarsaModel():
 
         self.Q = self.initialize_policy()
 
-    def get_states(self):
-        high_array = [1.5, 1.5, 5, 5, 3.14, 5, 1, 1]
-        increase = 1
-        count = 0
-        for value in high_array:
-            count += int(value*increase)
-            increase += 5
-        return count*2
-
     def initialize_policy(self):
         """
         Inicializa la política del agente
         """
-        # p = (1/self.n_actions)
-        policy = np.zeros((self.n_states, self.n_actions))
+        p = (1/self.n_actions)
+        policy = np.ones((self.n_states, self.n_actions))*p
         return policy
 
     def convert_state(self, observation):
         """
         Convierte el estado del agente en un entero
         """
-        pass
         return observation
+    
 
     def get_action(self, state, episode, env):
         '''
@@ -42,20 +33,20 @@ class SarsaModel():
         de la tabla Q
         '''
         new_epsilon = self.epsilon * 1 / ((episode * 0.1) + 1)
-        if new_epsilon < 0.01:
-            new_epsilon = 0.01
+        if new_epsilon < 0.1:
+            new_epsilon = 0.1
         if np.random.rand() < new_epsilon:
             action = env.action_space.sample()
         else:
-            return np.argmax(self.Q[state])
+            action = env.action_space.sample()
+            #return np.argmax(self.Q[state])
         return action
 
     def update(self, current_state, action, reward, next_state, next_action):
         '''
         Actualiza la tabla Q
         '''
-        self.Q[current_state, action] += self.alpha * \
-            (reward + self.gamma *
-             self.Q[next_state, next_action] - self.Q[current_state, action])
-
+        self.Q[current_state, action] = self.Q[current_state, action] + self.alpha * \
+                (reward + self.gamma *
+                 self.Q[next_state, next_action] - self.Q[current_state, action])
         return self.Q
