52b7ec59c265579801105d27b6fb669c72b9e7a5
Santiago Saavedra <mssr.99@hotmail.com>
2023-06-02 18:14:30 -0400

    graph with dotted line

--- utils/create_graph.py
+++ utils/create_graph.py
@@ -9,17 +9,28 @@ def create_graph(data_with_mlp, data_without_mlp, data_danger_with_mlp, data_dan
 
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 6))
 
-    # Gráfico 1
-    ax1.plot(x_mlp_reward, y_mlp_reward, color='blue', label='con MLP')    
-    ax1.plot(x_reward, y_reward, color='green', label='sin MLP')
+    # Gráfico 1    
+    # ax1.plot(x_reward, y_reward, color='green', label='sin MLP')
+    fit_reward = np.polyfit(x_reward, y_reward, 2)
+    ax1.plot(x_reward, np.poly1d(fit_reward)(x_reward), color='green', linestyle="dashed", label='sin MLP')
+
+    # ax1.plot(x_mlp_reward, y_mlp_reward, color='blue', label='con MLP')
+    fit_mlp_reward = np.polyfit(x_mlp_reward, y_mlp_reward, 2)
+    ax1.plot(x_mlp_reward, np.poly1d(fit_mlp_reward)(x_mlp_reward), color='blue', linestyle="dashed" ,label='con MLP')
     ax1.set_title('Relación entre Episodio y Recompensa')
     ax1.set_xlabel('Episodio')
     ax1.set_ylabel('Recompensa')
     ax1.legend()
 
-    # Gráfico 2
-    ax2.plot(x_mlp_danger, y_mlp_danger, color='blue', label='con MLP')
-    ax2.plot(x_danger, y_danger, color='green', label='sin MLP')
+    # Gráfico 2    
+    # ax2.plot(x_danger, y_danger, color='green', label='sin MLP')
+    fit_danger = np.polyfit(x_danger, y_danger, 2)
+    ax2.plot(x_danger, np.poly1d(fit_danger)(x_danger), color='green', linestyle="dashed", label='sin MLP')
+
+    # ax2.plot(x_mlp_danger, y_mlp_danger, color='blue', label='con MLP')
+    fit_mlp_danger = np.polyfit(x_mlp_danger, y_mlp_danger, 2)
+    ax2.plot(x_mlp_danger, np.poly1d(fit_mlp_danger)(x_mlp_danger), color='blue', linestyle="dashed", label='con MLP')
+    
     ax2.set_title('Relación entre Episodio y Estado peligroso')
     ax2.set_xlabel('Episodio')
     ax2.set_ylabel('Estado Peligroso')
